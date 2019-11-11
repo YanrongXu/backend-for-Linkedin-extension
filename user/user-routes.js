d35112df5a1c897b647cfb36750c40d51ee86510
Yanrong Xu <8xuyanrong@gmail.com>
2019-11-11 16:25:15 -0500

    update user

--- user/user-routes.js
+++ user/user-routes.js
@@ -35,55 +35,5 @@ router.delete('/banish', (req, res) => {
     })
 })
 
-// check if user is pro
-router.get('/upgrade/:id', async (req, res) => {
-  const id = req.params.id
-
-  if (!id) {
-    res.status(400).json({ message: 'Please provide user id.' })
-  }
-
-  const status = await User.getStatus(id)
-  console.log(status, 'status')
-  let count = await User.getFormCount(id)
-  count = parseInt(count.count, 10)
-
-  console.log(count, 'count')
-
-  if (status.pro == false && count >= 1) {
-    res.status(200).json({
-      message: 'Upgrade account to pro for more forms',
-      pro: status.pro,
-      form_count: count
-    })
-  } else {
-    res.status(200).json({
-      message: 'User can make forms',
-      pro: status.pro,
-      form_count: count
-    })
-  }
-})
-
-// upgrade user
-router.post('/upgrade', (req, res) => {
-  const { user_id } = req.body
-
-  if (!user_id) {
-    res.status(400).json({ message: 'Please provide user id.' })
-  }
-
-  User.upgradeUser(user_id)
-    .then(response => {
-      console.log(response, 'res')
-      res
-        .status(200)
-        .json({ message: 'user has been upgraded to a pro account!' })
-    })
-    .catch(error => {
-      console.log(error, 'er')
-      res.status(500).json({ err: 'Could not upgrade user', error })
-    })
-})
 
 module.exports = router
