e0fda20345d9ebdf63e3450e3dedaff9a9b50899
Yanrong Xu <8xuyanrong@gmail.com>
2019-11-11 16:12:02 -0500

    another user update

--- user/user-routes.js
+++ user/user-routes.js
@@ -4,53 +4,15 @@ const bcrypt = require('bcryptjs')
 const axios = require('axios')
 
 // // verify if user exists, if so return user if not add user then return user
-// router.get('/user', async (req, res) => {
-//   try {
-//     const users = await Users.find(req.query)
-//     res.status(200).json(users)
-//   } catch (error) {
-//     res.status(500).json(error)
-//   }
-// })
-
-// verify if user exists, if so return user if not add user then return user
-router.post('/user', async (req, res) => {
-  const response = await axios.get('https://api.linkedin.com/v2/me', {
-    headers: {
-      Authorization: 'Bearer ' + req.body.access_token,
-      Connection: 'Keep-Alive'
-    }
-  })
-
-  const { id } = response.data
-  const firstName = response.data.localizedFirstName
-  const lastName = response.data.localizedLastName
-
-  console.log(id)
-  console.log(firstName)
-  console.log(lastName)
-
-  if (!id) {
-    res.status(400).json({ message: 'Please provide user id.' })
+router.get('/user', async (req, res) => {
+  try {
+    const users = await Users.find(req.query)
+    res.status(200).json(users)
+  } catch (error) {
+    res.status(500).json(error)
   }
-
-  User.verifyUser(id)
-    .then(async user => {
-      const token = User.generateToken()
-      if (user == null) {
-        const newUser = await User.addUser({
-          user_id: id,
-          first_name: firstName,
-          last_name: lastName
-        })
-        res.status(200).json({ userInfo: newUser, token: token })
-      }
-      res.status(200).json({ userInfo: user, token: token })
-    })
-    .catch(error => {
-      res.status(500).json({ err: 'Could not verify user', error })
-    })
 })
+
 
 // delete user
 router.delete('/banish', (req, res) => {
